be87276a6c95e39beb65319ad04ce054b9723777
Florian Brunswicker <fbrunswi@adobe.com>
2024-06-17 14:44:07 +0200

    social links & social links in contributors block

--- blocks/contributors/contributors.js
+++ blocks/contributors/contributors.js
@@ -1,17 +1,34 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import {
+  loadFragment,
+} from '../fragment/fragment.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  for (const row of block.children) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'contributors-card-image';
-      else div.className = 'contributors-card-body';
-    });
+    for (const div of li.children) {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'contributors-card-image';
+      } else if (div.children.length === 1 && div.querySelector('a')) {
+        const link = div.querySelector('a');
+        const path = link ? link.getAttribute('href') : div.textContent.trim();
+        const [fragment] = await Promise.all([loadFragment(path)]);
+        if (fragment) {
+          const fragmentSection = fragment.querySelector(':scope .section');
+          if (fragmentSection) {
+            div.classList.add(...fragmentSection.classList);
+            div.replaceWith(...fragment.childNodes);
+          }
+        }
+      } else div.className = 'contributors-card-body';
+    }
     ul.append(li);
-  });
+  }
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
---
+++ blocks/social/social.js
@@ -0,0 +1,14 @@
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+  ul.classList.add('social-container');
+  // eslint-disable-next-line no-plusplus
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    const name = row.children[0].textContent;
+    const link = row.children[1].textContent;
+    li.innerHTML = `<div><a href="${link}" class="fa fa-brands fa-${name}" target="_blank"></a></div>`;
+    ul.append(li);
+  });
+  block.textContent = '';
+  block.append(ul);
+}
